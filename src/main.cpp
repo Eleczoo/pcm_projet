d87e5f85459d194fd46fd87e25e23c1c24ce3596
ThePurpleOne <jonas@stirnemann.xyz>
2025-01-01 14:22:12 +0100

    Cleaned up a bit

--- src/main.cpp
+++ src/main.cpp
@@ -56,6 +56,7 @@ uint32_t limit_max_path;
 volatile uint64_t g_total_verified = 0;
 
 
+
 // ! PROTOTYPES
 void worker_routine(int id);
 static void branch_and_bound(Path* current);
@@ -122,11 +123,23 @@ int main(int argc, char* argv[])
 
 	Path* current = new Path(g_graph);
 	current->add(0);
+	// g_fifo.enqueue(current);
 
-	g_fifo.enqueue(current);
+
+	Path* new_p;
+	for(int x = 0; x < g_graph->size(); x++)
+	{
+		new_p = new Path(g_graph);
+		new_p->copy(current);
+		if (!current->contains(x))
+		{
+			new_p->add(x);
+			g_fifo.enqueue(new_p);
+		}
+	}
+
 
 	// ! WORKERS
-	// std::cout << "Starting " << nb_threads << " threads\n";
 	std::thread workers[nb_threads];
 	for (uint32_t i = 0; i < nb_threads; i++)
 		workers[i] = std::thread(worker_routine, i);
@@ -134,12 +147,8 @@ int main(int argc, char* argv[])
 	for (uint32_t i = 0; i < nb_threads; i++)
 		workers[i].join();
 
-	//std::cout << COLOR.RED << "shortest " << global.shortest << COLOR.ORIGINAL << '\n';
 	std::cout << "shortest " << global.shortest << '\n';
-
-	// if (global.verbose & VER_COUNTERS)
-	// print_counters();
-
+	
 	return 0;
 }
 
@@ -153,96 +162,47 @@ void atomic_set(uint32_t* addr, uint32_t curr, uint32_t next)
 
 void worker_routine(int id)
 {
-	// Print timestamp in hh:mm:ss::ms
-	// std::cout << id  <<" - " << "STARTED WORKER " << id << std::endl;
 	Path* p;
 	Path* new_p;
 
 	while (1)
 	{
-
-		// if we processed every possible path, stop the threads
-		uint64_t cleared_paths = 0;
-		// g_mutex.lock();
-
-		
-		//auto start = std::chrono::system_clock::now();
-		// for (uint64_t i=0; i<global.size; i++) 
-		// {
-		// 	uint64_t e = global.fact[i] * global.counter.bound[i];
-
-		// 	cleared_paths += e;
-		// }
-		//auto end = std::chrono::system_clock::now();
-
-		//printf("Time taken by factorial (ns) : %lld\n", std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
-
 		//  ! ----- STOP CONDITION ----- ! 
-		// if((global.counter.verified + cleared_paths) >= global.total)
 		if(g_total_verified >= global.total)
 			break;
 
-
-		// ! 1. Dequeue a job
 		p = g_fifo.dequeue();
-
 
 		if(p == nullptr)
 		{
-			usleep(50);
+			usleep(100);
 			continue;
 		}
 		
-		//auto start = std::chrono::system_clock::now();
 		// ? Check if the distance is already bigger than the min
 		if (p->distance() > global.shortest->distance()) 
 		{
-			//___atomic_fetch_add(&global.counter.bound[p->size()], 1, __ATOMIC_RELAXED);
 			__atomic_fetch_add(&g_total_verified, global.fact[p->size()], __ATOMIC_RELAXED);
 			delete p;
-
-			// for (uint64_t i=0; i<global.size; i++) 
-			// {
-			// 	uint64_t e = global.fact[i] * global.counter.bound[i];
-
-			// 	cleared_paths += e;
-			// }
-			// __atomic_store_n(&g_total_verified, (global.counter.verified + cleared_paths), __ATOMIC_RELAXED);
-			// printf("%d - g_total_verified = %d\n", id, g_total_verified);
-
 			continue; 
 		}
-		//auto end = std::chrono::system_clock::now();
-
-		//printf("Time taken by bound (ns) : %lld\n", std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
-
 
 		// ! 2. Check if we need to split the job
 		if (p->size() >= (p->max() - (int)limit_max_path))
 		{
+			// ? Limit reached, Actually do the job
 			branch_and_bound(p);
-
-			// for (uint64_t i=0; i<global.size; i++) 
-			// {
-			// 	uint64_t e = global.fact[i] * global.counter.bound[i];
-
-			// 	cleared_paths += e;
-			// }
-			// __atomic_store_n(&g_total_verified, (global.counter.verified + cleared_paths), __ATOMIC_RELAXED);
-			// printf("%d - g_total_verified = %d\n", id, g_total_verified);
-
 		}
 		else
 		{
+			// ? Limit not reached, split the job
 			for(int x = 0; x < g_graph->size(); x++)
 			{
 				if(!p->contains(x))
 				{
 					new_p = new Path(g_graph);
 					new_p->copy(p);
-
 					new_p->add(x);
-
 					g_fifo.enqueue(new_p);
 				}
 			}
@@ -258,10 +218,7 @@ static void branch_and_bound(Path* current)
 
 	if (current->leaf()) 
 	{
-		//printf("LEAF\n");
-		// this is a leaf
 		current->add(0);
-		//__atomic_fetch_add(&global.counter.verified, 1, __ATOMIC_RELAXED);
 		__atomic_fetch_add(&g_total_verified, 1, __ATOMIC_RELAXED);
 
 
@@ -295,7 +252,7 @@ static void branch_and_bound(Path* current)
 		{
 			if (global.verbose & VER_BOUND )
 				std::cout << "bound " << current << '\n';
-			//__atomic_fetch_add(&global.counter.bound[current->size()], 1, __ATOMIC_RELAXED);
+
 			__atomic_fetch_add(&g_total_verified, global.fact[current->size()], __ATOMIC_RELAXED);
 		}
 
@@ -314,7 +271,6 @@ void reset_counters(int size)
 		global.counter.bound[i] = 0;
 		if (i) {
 			int pos = global.size - i;
-			//global.fact[pos] = (i-1) ? (i * global.fact[pos+1]) : 1;
 
 			if((i - 1) != 0)
 				global.fact[pos] = i * global.fact[pos+1];
@@ -327,9 +283,9 @@ void reset_counters(int size)
 
 void print_counters()
 {
-//	std::cout << "total: " << global.total << '\n';
-//	std::cout << "verified: " << global.counter.verified << '\n';
-//	std::cout << "found shorter: " << global.counter.found << '\n';
+	std::cout << "total: " << global.total << '\n';
+	std::cout << "verified: " << global.counter.verified << '\n';
+	std::cout << "found shorter: " << global.counter.found << '\n';
 	std::cout << "bound (per level):";
 	for (uint64_t i=0; i<global.size; i++)
 		std::cout << ' ' << global.counter.bound[i];
@@ -343,84 +299,3 @@ void print_counters()
 	std::cout << "\nbound equivalent (total): " << equiv << '\n';
 	std::cout << "check: total " << (global.total==(global.counter.verified + equiv) ? "==" : "!=") << " verified + total bound equivalent\n";
 }
-
-
-
-// ! GRAVEYARD
-
-//int main()
-//{
-//// 	DATA data = 12;
-//// 	DATA data2 = 69;
-
-// 	LockFreeQueue fifo = LockFreeQueue();
-
-//	// std::cout << "-----" << std::endl;
-//	// fifo.show_queue();
-//	// std::cout << "-----" << std::endl;
-
-//	// fifo.enqueue(&data);
-//	// fifo.show_queue();
-//	// std::cout << "-----" << std::endl;
-
-
-//	DATA data[20];
-//	for (int x = 0; x < 20; x++)
-//	{
-//		data[x] = x;
-//		fifo.enqueue(&data[x]);
-//	}
-//	std::cout << "Done queuing" << std::endl;
-
-//	// std::cout << "deq : " << *deq << std::endl;
-
-//	DATA* deq;
-//	for (int x = 0; x < 25; x++)
-//	{
-//		deq = fifo.dequeue();
-//		if (deq)
-//		{
-//			std::cout << "deq : " << *deq << std::endl;
-//		}
-//		else
-//		{
-//			std::cout << "fifo empty" << std::endl;
-//		}
-//	}
-//}
-
-
-
-
-
-
-// int main()
-// {
-// 	// DATA data = 12;
-// 	// DATA data2 = 69;
-
-// 	LockFreeQueue fifo = LockFreeQueue();
-
-// 	// std::cout << "-----" << std::endl;
-// 	// fifo.show_queue();
-// 	// std::cout << "-----" << std::endl;
-
-// 	// fifo.enqueue(&data);
-// 	// fifo.show_queue();
-// 	// std::cout << "-----" << std::endl;
-
-// 	// fifo.enqueue(&data2);
-// 	// fifo.show_queue();
-// 	// std::cout << "-----" << std::endl;
-
-// 	DATA data[20];
-// 	for (int x = 0; x < 20; x++)
-// 	{
-// 		data[x] = x;
-// 		fifo.enqueue(&data[x]);
-// 	}
-
-// 	fifo.show_queue();
-	
-// 	// std::cout << "show_queue" << std::endl;
-// }
